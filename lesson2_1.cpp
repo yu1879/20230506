3728c1814117255ff364462d7e44bc66209b3756
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-06 14:23:45 +0800

    C++

--- lesson2_1.cpp
+++ lesson2_1.cpp
@@ -2,6 +2,7 @@
 // Created by yu1879 on 2023/5/6.
 //
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main(int args ,char** argv) {
 
@@ -16,7 +17,8 @@ int main(int args ,char** argv) {
     int age=25;
     cout<<age<<endl;
     cout<<showpos<<age<<endl;
-
+float  score=123.456789;
+cout<<fixed<<setprecision(4)<<score<<endl;
     return 0;
 }
 
