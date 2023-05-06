81b9a3ad298a9855e3c91a0a3705ea13d3971883
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-06 14:10:35 +0800

    C++

---
+++ lesson2_1.cpp
@@ -0,0 +1,14 @@
+//
+// Created by yu1879 on 2023/5/6.
+//
+#include <iostream>
+using namespace std;
+int main(int args ,char** argv) {
+
+    bool flag=false;
+    cout<<flag<<endl;
+
+
+    return 0;
+}
+
