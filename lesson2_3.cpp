ff0e8e815993f4606debea949f377e323256c3bb
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-06 16:52:52 +0800

    C++

--- lesson2_3.cpp
+++ lesson2_3.cpp
@@ -4,16 +4,30 @@
 #include <iostream>
 using namespace std;
 int main(int args,char** argv ){
-int a=200;
-int * p;
-p=&a;
-cout<<"p的內容為:"<<p<<endl;
-    cout<<"a的位址為:"<<&a<<endl;
-    cout<<"a的內容為:"<<a<<endl;
-    cout<<"p儲存的位址所儲存的內容為:"<<*p<<endl;
-*p=500;
-    cout<<"p儲存的位址所儲存的內容為:"<<*p<<endl;
-    cout<<"a的內容為:"<<a<<endl;
+
+
+//int *p;
+//int *m;
+//int a=200;
+//*m=a;
+//p=&a;
+//m=p;
+//*m=666;
+//cout<<"a="<<a<<",*p的內容為:"<<*p<<"m的內容為:"<<*m<<endl;
+//  cout<<"p的內容為:"<<p<<endl;
+//    cout<<"m的內容為:"<<m<<endl;
+//    cout<<"a的位址為:"<<&a<<endl;
+
+    //int a=200;
+//int * p;
+//p=&a;
+//cout<<"p的內容為:"<<p<<endl;
+//    cout<<"a的位址為:"<<&a<<endl;
+//    cout<<"a的內容為:"<<a<<endl;
+//    cout<<"p儲存的位址所儲存的內容為:"<<*p<<endl;
+//*p=500;
+//    cout<<"p儲存的位址所儲存的內容為:"<<*p<<endl;
+//    cout<<"a的內容為:"<<a<<endl;
 
 //int a=200;
 //cout<<"a的內容:"<<a<<"a變數的地址為:"<<&a<<endl;
