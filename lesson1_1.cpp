c3eb2b68737c157b1327ecdf75fee5e27a94c3b5
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-06 13:27:44 +0800

    C++

---
+++ lesson1_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "myheader.h"
+using namespace std;
+using namespace pcschool;
+
+int main(int args ,char** argv) {
+//char name[30];
+//string name="";
+//cout<<"請輸入姓名:";
+//getline(cin,name);
+//cout<<name<<",歡迎光臨\n";
+//int a=2;
+//cout<<"a="<<a;
+//cout<<",a+1="<<++a<<endl;
+//  system("chcp 65001");
+//    cout<<RATE<<endl;
+//    int age;
+//   printf("請輸入年齡:");
+//    scanf("%d",&age);
+//   printf("您輸入的年齡為:%d歲\n",age);
+//    system("pause");
+//    system("cls");
+//    cout<<"請輸入年齡";
+//    cin>>age;
+//    cout<<"您輸入的年齡為:"<<age<<"歲"<<std::endl;
+//    return 0;
+}
