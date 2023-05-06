8f8f55654d4818f49532181a0dc1ae08d946ebec
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-06 14:15:24 +0800

    C++

--- lesson2_1.cpp
+++ lesson2_1.cpp
@@ -6,8 +6,16 @@ using namespace std;
 int main(int args ,char** argv) {
 
     bool flag=false;
+    cout<<boolalpha<<flag<<endl;
     cout<<flag<<endl;
-
+    cout<<flag<<endl;
+    cout<<flag<<endl;
+    cout<<flag<<endl;
+    cout<<flag<<endl;
+    cout<<noboolalpha<<flag<<endl;
+    int age=25;
+    cout<<age<<endl;
+    cout<<showpos<<age<<endl;
 
     return 0;
 }
