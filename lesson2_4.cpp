e9f44a5b06ed83e457a47a34592098ca9bee0ba2
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-06 17:11:14 +0800

    C++

---
+++ lesson2_4.cpp
@@ -0,0 +1,16 @@
+//
+// Created by yu1879 on 2023/5/6.
+//
+#include <iostream>;
+using namespace std;
+int main(){
+
+int lotteTypel,max ;
+int *lotto;
+cout<<"請選擇 \n 1.三星彩\n 2.四星彩 \n";
+cin>>lotteTypel;
+
+
+
+    return 0;
+}
