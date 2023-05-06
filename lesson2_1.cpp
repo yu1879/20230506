60d68134ca8d29bf5479813253e52bbb475e9332
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-06 15:10:42 +0800

    C++

--- lesson2_1.cpp
+++ lesson2_1.cpp
@@ -1,24 +1,61 @@
-//
-// Created by yu1879 on 2023/5/6.
-//
 #include <iostream>
 #include <iomanip>
-using namespace std;
-int main(int args ,char** argv) {
 
-    bool flag=false;
-    cout<<boolalpha<<flag<<endl;
-    cout<<flag<<endl;
-    cout<<flag<<endl;
-    cout<<flag<<endl;
-    cout<<flag<<endl;
-    cout<<flag<<endl;
-    cout<<noboolalpha<<flag<<endl;
-    int age=25;
-    cout<<age<<endl;
-    cout<<showpos<<age<<endl;
-float  score=123.456789;
-cout<<fixed<<setprecision(4)<<score<<endl;
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+    int max,sum=0;
+    bool flag;
+    do {
+        flag=false;
+        cout<<"設定終止值：";
+        cin>>max;
+        //判斷max是否介於2-100間
+        if(max>=2 && max<=100) {
+            for(int m=1; m<=max; m++) {
+                //判斷是否為奇數,若是則相加,後面符號為-
+                if(m%2!=0) {
+                    //判斷是否為最後一個數字
+                    if(m!=max)
+                        cout<<m<<"-";
+                    else
+                        cout<<m;
+                    //奇數相加
+                    sum+=m;
+                } else {
+                    //偶數
+                    if(m!=max)
+                        cout<<m<<"+";
+                    else
+                        cout<<m;
+                    //偶數相減
+                    sum-=m;
+                }
+            }
+            cout<<"="<<showpos<<sum<<endl;
+
+        } else {
+            flag=true;
+            cout<<"Range : 2 To 100\n";
+        }
+
+    } while(flag);
+
+//    bool flag=false;
+//    cout<<boolalpha<<flag<<endl;
+//    cout<<flag<<endl;
+//    cout<<flag<<endl;
+//    cout<<flag<<endl;
+//    cout<<flag<<endl;
+//    cout<<flag<<endl;
+//    cout<<noboolalpha<<flag<<endl;
+//    int age=25;
+//    cout<<age<<endl;
+//    cout<<showpos<<age<<endl;
+//float  score=123.456789;
+//cout<<fixed<<setprecision(4)<<score<<endl;
+
     return 0;
 }
-
